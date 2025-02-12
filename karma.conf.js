8e121e5563d1f7e756bab0619a8c353aecb6e486
Miha Primc <miha.primc@beenius.tv>
2025-02-12 15:01:13 +0100

    Update karma test with ChromeHeadless browser

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage')
+    ],
+    client: {
+      clearContext: false
+    },
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/'
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    restartOnFileChange: true
+  })
+}
