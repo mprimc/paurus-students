8cc769d386ed178887175920f529058ed99b4267
Miha Primc <miha.primc@beenius.tv>
2025-02-13 11:16:30 +0100

    Integrate cypress image snapshot into e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'cypress'
+import { addMatchImageSnapshotPlugin } from '@simonsmith/cypress-image-snapshot/plugin'
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
-    supportFile: false,
     specPattern: 'cypress/e2e/**/*.cy.ts',
     video: false,
     screenshotOnRunFailure: true,
@@ -11,6 +11,9 @@ export default defineConfig({
       runMode: 2,
       openMode: 0
     },
-    defaultCommandTimeout: 10000
+    defaultCommandTimeout: 10000,
+    setupNodeEvents(on) {
+      addMatchImageSnapshotPlugin(on)
+    }
   }
 })
---
+++ cypress/support/e2e.ts
@@ -0,0 +1,5 @@
+import { addMatchImageSnapshotCommand } from '@simonsmith/cypress-image-snapshot/command'
+
+addMatchImageSnapshotCommand({
+  failureThreshold: 0.2
+})
